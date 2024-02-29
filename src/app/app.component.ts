6d593e2d4b66f57eec190d400af053508400a818
Siew Wing Fei <wingfei.siew@nova-hub.com>
2024-02-29 20:54:34 +0800

    k

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'mynav';
+  selectedWing = 'macalister';
   listFloors = ['GF', '1F', '2F', '3F', '5F', '7F', '8F'];
   selectedFloor = 'GF';
   fromX = 'Registration';
@@ -20,7 +21,8 @@ export class AppComponent {
       fx1: 'GF',
       fx2: '2F',
       im1: 'x1',
-      im2: 'x2'
+      im2: 'x2',
+      w: 'macalister'
     }
   ];
 
@@ -34,7 +36,7 @@ export class AppComponent {
   }
 
   hasNavFrom(fx: string) {
-    let r = this.listNav.find(k => k.from === this.fromX);
+    let r = this.listNav.find(k => k.from === this.fromX && this.selectedWing === 'macalister');
     if (r) {
       return r.fx1 === fx;
     }
@@ -43,7 +45,7 @@ export class AppComponent {
   }
 
   hasNavTo(fx: string) {
-    let r = this.listNav.find(k => k.to === this.toX);
+    let r = this.listNav.find(k => k.to === this.toX && this.selectedWing === 'macalister');
     if (r) {
       return r.fx2 === fx;
     }
@@ -53,7 +55,7 @@ export class AppComponent {
 
   getFloor() {
     let f = this.selectedFloor.toLowerCase();
-    let r = this.listNav.find(k => k.from === this.fromX && k.to === this.toX);
+    let r = this.listNav.find(k => k.from === this.fromX && k.to === this.toX && k.w === this.selectedWing);
     if (r) {
       if (this.selectedFloor === r.fx1) {
         return r.im1;
@@ -65,5 +67,18 @@ export class AppComponent {
     }
 
     return f;
+  }
+
+  onSelectWing(w: string) {
+    this.selectedWing = w;
+    if (w === 'macalister') {
+      this.listFloors = ['GF', '1F', '2F', '3F', '5F', '7F', '8F'];
+      this.selectedFloor = 'GF';
+    }
+
+    else {
+      this.listFloors = ['BF', '1F', '2F', '3F', '3AF', '5F', '6F', '7F'];
+      this.selectedFloor = 'BF';
+    }
   }
 }
